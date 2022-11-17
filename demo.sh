4ece0e4ea6aa66fcf1a64784c48fe5d990e6ec49
Dhaneesh <dhaneesh88@gmail.com>
2022-11-17 22:56:21 +0100

    Added changes.~

---
+++ demo.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Hello world";
+echo "This is a shell .sh file for demo";
+// your shell commands go here
---
+++ process.php
@@ -0,0 +1,10 @@
+<?php
+
+//if(isset($_POST['username'])){
+    echo $result = shell_exec('sh demo.sh');
+//}
+
+//Print the return value
+echo $result;
+
+die();
